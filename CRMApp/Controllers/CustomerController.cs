cd59c53275f6ff1800e1732caf0a178cc85a19ba
AshwinKushwaha <ashwin.kushwaha@mrisoftware.com>
2025-09-19 18:14:08 +0530

    Created customer list and add customer page
    added the pages and handled the authorization for these pages

--- CRMApp/Controllers/CustomerController.cs
+++ CRMApp/Controllers/CustomerController.cs
@@ -1,4 +1,6 @@
 ﻿using CRMApp.Areas.Identity.Data;
+using CRMApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,15 +10,43 @@ namespace CRMApp.Controllers
     {
         private readonly ApplicationUserIdentityContext context;
 
+
         public CustomerController(ApplicationUserIdentityContext context)
         {
             this.context = context;
         }
 
+        [Authorize(Roles = "admin , salesrep, support")]
         public IActionResult Index()
         {
             var cust = context.Customers.ToList();
+            ViewBag.Count = cust.Count;
             return View(cust);
+        }
+
+
+        [Authorize(Roles = "admin, salesrep")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(cust);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [Authorize(Roles = "admin, salesrep")]
+        public IActionResult Edit()
+        {
+            return View();
         }
     }
 }
--- CRMApp/Models/Customer.cs
+++ CRMApp/Models/Customer.cs
@@ -1,17 +1,28 @@
-﻿namespace CRMApp.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRMApp.Models
 {
     public class Customer
     {
+        [Key]
+        [BindNever]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Phone { get; set; }
 
+        [BindNever]
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+
+        [BindNever]
+        public DateTime? UpdatedAt { get; set; }
 
     }
 }
